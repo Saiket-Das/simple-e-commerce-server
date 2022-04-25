e87d5bd45376f6ca36b20a1c6889ccd23ce1c326
Saiket Das <saiketdas02@gmail.com>
2022-04-25 14:10:44 +0800

    Server side Modify

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 const express = require('express');
 const cors = require('cors');
@@ -16,6 +17,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        await client.connect()
+        const productsCollection = client.db('emajhon').collection('product')
+
+        app.get('/product', async (req, res) => {
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = productsCollection.find(query);
+            let products;
+            if (page || size) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+
+            res.send(products);
+        });
+        app.get('/productCount', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const count = await cursor.count();
+            res.send({ count })
+        })
+
+        app.post('/productByKeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id));
+            const query = { _id: { $in: ids } }
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            console.log(keys);
+            res.send(products);
+        })
 
     }
     finally {
